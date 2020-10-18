fa2d057824b42c5fa072674e9458a69bd2ee1477
Ori Shadmon <oshadmon@gmail.com>
2020-10-18 15:03:11 -0700

    value range * 10

--- ping_sensor.py
+++ ping_sensor.py
@@ -7,32 +7,32 @@ PING_DATA = {
         'ADVA FSP3000R7': {
             'parentelement': str(uuid.uuid4()),
             'webid': 'F1AbEfLbwwL8F6EiShvDV-QH70AkxjnYuCS6RG0ZdSFZFT0ugnMRtEzvxdFojNpadLPwI4gWE9NUEFTUy1MSVRTTFxMSVRTQU5MRUFORFJPXDc3NyBEQVZJU1xQT1AgUk9PTVxBRFZBIEZTUDMwMDBSN3xQSU5H',
-            'min_value': 0,
-            'max_value': 4
+            'min_value': 10,
+            'max_value': 40
         },
         'Ubiquiti OLT': {
             'parentelement': str(uuid.uuid4()),
             'webid': 'F1AbEfLbwwL8F6EiShvDV-QH70Ay9wV1b5Y6hG0bdSFZFT0ugxACfpGU7d1ojPpadLPwI4gWE9NUEFTUy1MSVRTTFxMSVRTQU5MRUFORFJPXDc3NyBEQVZJU1xQT1AgUk9PTVxVQklRVUlUSSBPTFR8UElORw',
-            'min_value': 0,
-            'max_value': 9
+            'min_value': 10,
+            'max_value': 90
         },
         'VM Lit SL NMS': {
             'parentelement': str(uuid.uuid4()),
             'webid': 'F1AbEfLbwwL8F6EiShvDV-QH70ATrGzGrGT6RG0ZdSFZFT0ugQW05a2rwdFojNpadLPwI4gWE9NUEFTUy1MSVRTTFxMSVRTQU5MRUFORFJPXDc3NyBEQVZJU1xQT1AgUk9PTVxGLk8gTU9OSVRPUklORyBTRVJWRVJcVk0gTElUIFNMIE5NU3xQSU5H',
-            'min_value': 0,
-            'max_value': 14
+            'min_value': 10,
+            'max_value': 140
         },
         'Catalyst 3500XL': {
             'parentelement': str(uuid.uuid4()),
             'webid': 'F1AbEfLbwwL8F6EiShvDV-QH70A74uuaOGS6RG0ZdSFZFT0ug4FckGTrxdFojNpadLPwI4gWE9NUEFTUy1MSVRTTFxMSVRTQU5MRUFORFJPXDc3NyBEQVZJU1xQT1AgUk9PTVxDQVRBTFlTVCAzNTAwWEx8UElORw',
-            'min_value': 0,
-            'max_value': 44
+            'min_value': 10,
+            'max_value': 440
         },
         'GOOGLE_PING': {
             'parentelement': str(uuid.uuid4()),
             'webid': 'F1AbEfLbwwL8F6EiShvDV-QH70AMgi98B6o6hG0bdSFZFT0ugPdQ3gcXLd1ojPpadLPwI4gWE9NUEFTUy1MSVRTTFxMSVRTQU5MRUFORFJPXDc3NyBEQVZJU1xHT09HTEVfUElOR3xQSU5H',
-            'min_value': 2,
-            'max_value': 88
+            'min_value': 20,
+            'max_value': 880
         }
 }
 
