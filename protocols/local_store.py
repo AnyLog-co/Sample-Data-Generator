ce1a12d502ba252e666e7a9793ce0892c51a6176
Ori Shadmon <oshadmon@gmail.com>
2020-12-11 20:51:42 +0000

    local file

---
+++ protocols/local_store.py
@@ -0,0 +1,81 @@
+import json
+import os 
+
+def __convert_json(payload:dict)->str: 
+    """
+    Convett dict to JSON 
+    :args: 
+        payload:dict 
+    :return: 
+       paylaod converted to string 
+    """
+    try: 
+        return json.dumps(payload)
+    except Exception as e: 
+        print('Failed to convert JSON') 
+        return None 
+
+def __create_dir(dir_path:str)->str: 
+    """
+    Expand path + validate dir exists
+    :args: 
+        dir_path:str - directory path 
+    :return: 
+        extracted dir_path 
+    """
+    dir_path = os.path.expanduser(os.path.expandvars(dir_path))
+    if not os.path.isdir(dir_path): 
+        os.makedirs(dir_path)
+    return dir_path 
+
+def print_store(payloads:list)->bool: 
+    """
+    Print rows to screen as JSON objects
+    :args: 
+        payloads:dict - payloads
+    :param: 
+        output:None - object to validate JSON worked 
+    :return: 
+        If fails to convert to json returns False 
+    """
+    output = None 
+    for payload in payloads: 
+        output = __convert_json(payload)
+        if output is None: 
+           return False
+        print(output) 
+    return True 
+
+def file_store(payloads:list, dbms:str, table_name:str, prep_dir:str, watch_dir:str)->bool:
+    """
+    Store data in file 
+    :args: 
+        payloads:list - data to store in file 
+        dbms:str - logical databse to store data in 
+        table_name: - logical table name to store data in 
+        prep_dir:str - dir to prep data in 
+        watch_dir:str - dir ready to be stored on AnyLog 
+    :param: 
+        device_id:str - device id for file name 
+        timestamp:str - timestamp for file 
+        file_name:str - file to store data in 
+    """
+    prep_dir = __create_dir(prep_dir)
+    watch_dir = __create_dir(watch_dir)
+
+    device_id = payloads[0]['parentelement'].replace('-', '')
+    timestamp = payloads[0]['timestamp'].replace('-', '').replace(' ', '').replace(':', '').replace('.', '')
+
+    file_name = '%s/%s.%s.%s.%s.json' % (prep_dir, dbms, table_name, device_id, timestamp) 
+    open(file_name, 'w').close() 
+
+    for payload in payloads: 
+        output = __convert_json(payload) 
+        with open(file_name, 'a',) as f: 
+            f.write('%s\n' % output) 
+
+    if watch_dir is not None: 
+        os.rename(file_name, file_name.replace(prep_dir, watch_dir))
+        print('Data located in %s' % file_name.replace(prep_dir, watch_dir)) 
+    else: 
+        print('Data located in %s' % file_name)
--- test.py
+++ test.py
@@ -1,17 +1,63 @@
+import time
+
 from data_generators import machine_info, percentagecpu_sensor, ping_sensor, trig
-from protocols import rest_protocol,  store_data_options,  write_file
+from protocols import rest_protocol, local_store
 
 
-print(ping_sensor.get_ping_data()) 
-print(percentagecpu_sensor.get_percentagecpu_data())
+def get_data(sensor:str, row_count:int, sleep:float)->list: 
+    """
+    Based on sensor type get set values
+    :args: 
+        sensors:str - type of sensor [machine, ping, sin, cos, rand] 
+        rows:int - number of data sets to generate for machine / ping sensor. All others have 30 rows by default 
+        sleep:float - wait time between each row 
+    :param: 
+        rows:list - rows genenrated
+    :return: 
+        rows
+    """
+    rows = [] 
+    if sensor == 'machine': 
+        for row in range(row_count): 
+            rows.append(machine_info.get_device_data())
+            time.sleep(sleep) 
+    elif sensor == 'ping': 
+        for row in range(row_count): 
+            rows.append(ping_sensor.get_ping_data()) 
+            time.sleep(sleep) 
+    elif sensor == 'sin':  
+        rows = trig.sin_value(0)
+    elif sensor == 'cos': 
+        rows = trig.cos_value(0)
+    elif sensor == 'rand': 
+         rows = trig.rand_value(0)
+    return rows 
 
-print(machine_info.get_device_data())
+def store_data(payloads:list, conn:str, dbms:str, table_name:str, store_type:str, rest_format:str, prep_dir:str, watch_dir:str)->bool:
+    """
+    Store data
+    :args: 
+        payloads:list - Data to store 
+        conn:str - IP + Port (required for REST only) 
+        dbms:str - logical database name (required for REST + file) 
+        table_name:str - logical table tname (required for REST + file)
+        store_type:str - Format by which to store (print, file, REST) 
+        rest_format:str - Format by which to send REST (file or streaming) 
+        prep_dir:str - Directory to prep data in (file only) 
+        watch_dir:dir - Directory data ready to be sent onto AnyLog (file only) 
+    :param: 
+        status:bool - status 
+    """
+    status = True 
+    if store_type == 'rest': 
+        status = rest_protocol.send_rest(payloads, conn, dbms, table_name, rest_format)
+    elif store_type == 'file': 
+        status = local_store.file_store(payloads, dbms, table_name, prep_dir, watch_dir) 
+    elif store_type == 'print': 
+        status = local_store.print_store(payloads) 
 
-print(trig.sin_value(0))
-print(trig.cos_value(0))
-print(trig.rand_value(0))
+    return status  
 
-
-
-
-
+if __name__ == '__main__': 
+    payloads = get_data('ping', 10, 0) 
+    store_data(payloads=payloads, conn=None, dbms='sample_data', table_name='ping_sensor', store_type='file', rest_format=None, prep_dir='$HOME/data/prep', watch_dir='$HOME/data/watch') 
