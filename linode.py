8c6e94493e66d4fb4fab132b7d8ade91e1ffd229
Ori Shadmon <oshadmon@gmail.com>
2021-07-20 18:15:39 +0000

    linode code

--- linode.py
+++ linode.py
@@ -4,47 +4,26 @@ import json
 import requests 
 import time 
 
-def get_data(token:str)->dict:
+def get_data(url:str, token:str)->dict:
     """
     Get data from linode
     :args:
+        url:str - URL to get data from 
         token:str - REST linode API Token 
     :params: 
         output:dict - data from GEt request 
     :return: 
         output 
-    :sample-data:
-    {
-        'region': 'ap-south', 
-        'specs': {
-            'disk': 25600, 'memory': 1024, 'transfer': 1000, 
-            'gpus': 0, 'vcpus': 1
-        }, 
-        'hypervisor': 'kvm', 'label': 'lsl-publisher', 'created': '2020-02-28T03:56:40', 
-        'alerts': {
-            'transfer_quota': 80, 'network_in': 10, 'io': 10000, 'network_out': 10, 'cpu': 90
-        }, 
-        'image': 'linode/ubuntu16.04lts', 'tags': ['demo'], 'id': 19564453, 'type': 'g6-nanode-1', 
-        'watchdog_enabled': True, 'ipv4': ['172.104.180.110'], 'ipv6': '2400:8901::f03c:92ff:fee5:d553/128', 
-        'status': 'running', 'group': '', 
-        'backups': {
-            'enabled': False, 
-            'schedule': {
-                'day': None, 'window': None
-            }, 
-            'last_successful': None
-        }, 
-        'updated': '2021-03-26T17:59:19'
-    } 
     """
     try:
-        r = requests.get('https://api.linode.com/v4/linode/instances?data', headers={'Authorization': 'Bearer %s' % token}) 
+        #r = requests.get('https://api.linode.com/v4/linode/instances?data', headers={'Authorization': 'Bearer %s' % token}) 
+        r = requests.get(url, headers={'Authorization': 'Bearer %s' % token}) 
     except Exception as e: 
         print('Failed to GET data from linode (Error: %s)' % e)
         output = None
     else: 
         if r.status_code != 200: 
-            print('Fialed to GET data form linode due to network error: %s' % str(r.status_code)) 
+            print('Failed to GET data form linode due to network error: %s' % str(r.status_code)) 
             output = None 
         else: 
             try: 
@@ -80,192 +59,112 @@ def post_data(conn:str, data:dict):
         nothing is returned. however if something fails message is printed
     """
     headers = {
-        'Content-Type': 'text/plain',
         'command': 'data',
-        'User-Agent': 'AnyLog/1.23'
-    }
-    jdata = __convert_json(data) 
-    if jdata != None: 
-        try: 
-            r = requests.post('http://%s' % conn, headers=headers, data=jdata)
-        except Exception as e: 
-            print('Failed to POST data to %s (Error: %s)' % (conn, e))
-        else: 
-            if r.status_code != 200: 
-                print('Failed to POST data to %s due to network error: %s' % (conn, r.status_code))
-      
-def useage(data:list, dbms:str)->list: 
+        'User-Agent': 'AnyLog/1.23',
+        'Content-Type': 'text/plain'
+    }    
+    try: 
+        r = requests.post('http://%s' % conn, headers=headers, data=data)
+    except Exception as e: 
+        print('Failed to POST data to %s (Error: %s)' % (conn, e))
+    else: 
+        if r.status_code != 200: 
+           print('Failed to POST data to %s due to network error: %s' % (conn, r.status_code))
+
+def __extract_nodes(data:list)->dict:
     """
-    Extract useage config from GET result set 
+    Using data from Linode API extract node names and ids 
     :args: 
-        data:list - data from GET request 
-        dbms:str - database to store data in
+        data:str - data to extract info from
     :param: 
-        rows:list - list of JSON dict objects to AnyLog
+        nodes:dict - node_name:node_id
     :return: 
-        rows
-    :mqtt-call:
-    # To execute within file, MQTT call should be within 1 line without "<" or ">" 
-    <run mqtt client where broker=rest and user-agent=anylog and log=false and topic=(
-        name=anylog and dbms="bring [dbms]" and table="bring [table]" and 
-        column.timestamp.timestamp="bring [timestamp]" and 
-        column.node_id.int="bring [node_id]" and
-        column.node_name.str="bring [node_name]" and 
-        column.value.float="bring [value]" 
-    )> 
-    # This example is based on the commented out row 
-    <run mqtt client where broker=rest and user-agent=anylog and log=false and topic=(
-        name=anylog and dbms="bring [dbms]" and table="bring [table]" and 
-        column.timestamp.timestamp="bring [timestamp]" and 
-        column.node_id.int="bring [node_id]" and
-        column.node_name.str="bring [node_name]" and 
-        column.disk_io.float="bring [io]" and 
-        column.cpu.float="bring [cpu]" and 
-        column.network_in.float="bring [network_in]" and 
-        column.network_out.float="bring [network_out]" and 
-        column.transfer_quota.float="bring [transfer_quota]"
-    )> 
+        nodes
     """
-    rows = [] 
-    rows.append({
-                'dbms': dbms, 
-                'table': 'mahine_useage', 
-                'timestamp': node['updated'],
-                'node_id': node['id'], 
-                'node_name': node['label'],
-                'io': node['alerts']['io'], 
-                'cpu': node['alerts']['cpu'], 
-                'network_in': node['alerts']['network_in'],
-                'network_out': node['alerts']['network_out'],
-                'transfer_quota': node['alerts']['transfer_quota']
-    })
-    return rows
-
-def disk_io(data:list, dbms:str): 
-    rows = []
+    nodes = {} 
     for node in data: 
         if 'demo' in node['tags'] and isinstance(node, dict): 
-            row = {
-                'dbms': dbms, 
-                'table': None, 
-                'timestamp': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'),
-                'node_name': 'unknown',
-                'node_id': 0, 
-                'value': None
-            } 
-            if 'updated' in list(node.keys()):
-                row['timestamp'] = node['updated']
-            if 'id' in list(node.keys()):
-                row['node_id'] = node['id']
-            if 'label' in list(node.keys()):
-                row['node_name'] = node['label'] 
-            if 'io' in node['alerts']:
-                row['table'] = 'disk_io'
-                row['value'] = node['alerts']['io'] 
-                rows.append(row) 
+            if 'label' in list(node.keys()) and 'id' in list(node.keys()):
+                nodes[node['label']] = node['id'] 
+    return nodes 
 
-    return rows
+def cpu_insight(node:str, data:list, dbms:str, timestamp:str)->list: 
+    """
+    Calculate CPU
+    :args:
+        node:str - node name 
+        data:list - data for specific node 
+        dbms:str - database name 
+        table:str -  table name 
+    :param: 
+        cpu:list  - CPU data 
+        data:dict - data to POST 
+    :return: 
+        data 
+    """
+    insight = [] 
+    for row in data: 
+        insight.append(row[-1])
+    data = { 
+        'dbms': dbms, 
+        'table': 'cpu_insight', 
+        'node': node,
+        'timestamp': timestamp, 
+        'value': sum(insight)/len(insight) 
+    }
+    #return data 
+    return __convert_json(data) 
 
-def cpu_usage(data:list, dbms:str): 
-    rows = []
-    for node in data: 
-        if 'demo' in node['tags'] and isinstance(node, dict): 
-            row = {
-                'dbms': dbms, 
-                'table': None, 
-                'timestamp': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'),
-                'node_name': 'unknown',
-                'node_id': 0, 
-                'value': None
-            } 
-            if 'updated' in list(node.keys()):
-                row['timestamp'] = node['updated']
-            if 'id' in list(node.keys()):
-                row['node_id'] = node['id']
-            if 'label' in list(node.keys()):
-                row['node_name'] = node['label'] 
-            if 'cpu' in node['alerts']:
-                row['table'] = 'cpu_useage'
-                row['value'] = node['alerts']['cpu'] 
-                rows.append(row) 
+def io_insight(node:str, data:list, dbms:str, timestamp:str)->list: 
+    """
+    Swap data 
+    :args:
+        node:str - node name 
+        data:list - data for specific node 
+        dbms:str - database name 
+        table:str -  table name 
+    :param: 
+        cpu:list  - CPU data 
+        data:dict - data to POST 
+    :return: 
+        data 
+    """
+    insight = [] 
+    for row in data: 
+        insight.append(row[0])
 
+    data = { 
+        'dbms': dbms, 
+        'table': 'io_swap_insight', 
+        'node': node,
+        'timestamp': timestamp, 
+        'value': sum(insight)/len(insight) 
+    }
+    return __convert_json(data) 
 
+def process(token:str, dbms:str)->list: 
+    """
+    Call to get data
+    """
+    output = get_data('https://api.linode.com/v4/linode/instances?data', token) 
+    if isinstance(output, dict) and 'data' in list(output.keys()):
+        nodes = __extract_nodes(output['data']) 
+    if isinstance(nodes, dict) and nodes != {}: 
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        data = {} 
+        for node in list(nodes.keys()):
+            output = get_data('https://api.linode.com/v4/linode/instances/%s/stats' % nodes[node], token) 
+            for key in list(output['data'].keys()):
+                if key == 'cpu': 
+                    value = cpu_insight(node, output['data'][key], dbms, timestamp)
+                elif key == 'io': 
+                    value = io_insight(node, output['data'][key]['swap'], dbms, timestamp)
+                if key in ['cpu', 'io']: 
+                    if key not in data: 
+                        data[key] = []
+                    data[key].append(value) 
 
-    return rows
- 
-def network_in(data:list, dbms:str): 
-    rows = []
-    for node in data: 
-        if 'demo' in node['tags'] and isinstance(node, dict): 
-            row = {
-                'dbms': dbms, 
-                'table': None, 
-                'timestamp': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'),
-                'node_name': 'unknown',
-                'node_id': 0, 
-                'value': None
-            } 
-            if 'updated' in list(node.keys()):
-                row['timestamp'] = node['updated']
-            if 'id' in list(node.keys()):
-                row['node_id'] = node['id']
-            if 'label' in list(node.keys()):
-                row['node_name'] = node['label'] 
-            if 'network_in' in node['alerts']:
-                row['table'] = 'network_in'
-                row['value'] = node['alerts']['network_in'] 
-                rows.append(row) 
-    return rows 
-  
-def network_out(data:list, dbms:str): 
-    rows = []
-    for node in data: 
-        if 'demo' in node['tags'] and isinstance(node, dict): 
-            row = {
-                'dbms': dbms, 
-                'table': None, 
-                'timestamp': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'),
-                'node_name': 'unknown',
-                'node_id': 0, 
-                'value': None
-            } 
-            if 'updated' in list(node.keys()):
-                row['timestamp'] = node['updated']
-            if 'id' in list(node.keys()):
-                row['node_id'] = node['id']
-            if 'label' in list(node.keys()):
-                row['node_name'] = node['label'] 
-            if 'network_out' in node['alerts']:
-                row['table'] = 'network_out'
-                row['value'] = node['alerts']['network_out'] 
-                rows.append(row) 
-    return rows  
-  
-def transfer_quota(data:list, dbms:str): 
-    rows = []
-    for node in data: 
-        if 'demo' in node['tags'] and isinstance(node, dict): 
-            row = {
-                'dbms': dbms, 
-                'table': None, 
-                'timestamp': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'),
-                'node_name': 'unknown',
-                'node_id': 0, 
-                'value': None
-            } 
-            if 'updated' in list(node.keys()):
-                row['timestamp'] = node['updated']
-            if 'id' in list(node.keys()):
-                row['node_id'] = node['id']
-            if 'label' in list(node.keys()):
-                row['node_name'] = node['label'] 
- 
-            if 'transfer_quota' in node['alerts']:
-                row['table'] = 'transfer_quota'
-                row['value'] = node['alerts']['transfer_quota'] 
-                rows.append(row) 
- 
-    return rows 
+    return data 
 
 def main(): 
     """
@@ -284,6 +183,11 @@ def main():
         send_data:list - extracted data to send into AnyLog
     :URL: 
     https://www.linode.com/docs/api/
+    :sample: 
+    {"table": "io_swap_insight", "node": "anylog-query", "dbms": "dmci", "value": 1626595200000.0, "timestamp": "2021-07-18 20:0017.397818"}    
+
+    run mqtt client where broker=rest and user-agent = anylog and topic=(name=machine and dbms="bring [dbms]" and table="bring [table]" and column.timestamp.timestamp="bring [timestamp]" and column.node.str="bring [node]" and column.value.float="bring [value]") 
+
     """
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('conn',  type=str,  default='172.104.180.110:2049',                                             help='AnyLog REST IP & Port to send data to')
@@ -292,42 +196,20 @@ def main():
     parser.add_argument('-i', '--iteration', type=int,   default=1,                              help='number of iterations. if set to 0 run continuesly')
     parser.add_argument('-s', '--sleep',     type=float, default=0,                              help='wait between insert')
     args = parser.parse_args()
-      
+
     if args.iteration == 0: 
         while True: 
-            output = get_data(args.token) 
-            if 'data' in list(output.keys()): 
-                data = output['data']
-                send_data = disk_io(data=data, dbms=args.dbms)
-                post_data(conn=args.conn, data=send_data) 
-                send_data = cpu_usage(data=data, dbms=args.dbms) 
-                post_data(conn=args.conn, data=send_data) 
-                send_data = network_in(data=data, dbms=args.dbms) 
-                post_data(conn=args.conn, data=send_data) 
-                send_data = network_out(data=data, dbms=args.dbms)
-                post_data(conn=args.conn, data=send_data) 
-                send_data =transfer_quota(data=data, dbms=args.dbms) 
-                post_data(conn=args.conn, data=send_data) 
+            data = process(token=args.token, dbms=args.dbms)
+            for key in data: 
+                for row in data[key]: 
+                    post_data(conn=args.conn, data=row)
             time.sleep(args.sleep) 
-
-
-    for i in range(args.iteration):
-        output = get_data(args.token) 
-        print(output) 
-        if 'data' in list(output.keys()) and isinstance(output, dict): 
-            data = output['data']
-            send_data = disk_io(data=data, dbms=args.dbms)
-            post_data(conn=args.conn, data=send_data) 
-            send_data = cpu_usage(data=data, dbms=args.dbms) 
-            post_data(conn=args.conn, data=send_data) 
-            send_data = network_in(data=data, dbms=args.dbms) 
-            post_data(conn=args.conn, data=send_data) 
-            send_data = network_out(data=data, dbms=args.dbms)
-            post_data(conn=args.conn, data=send_data) 
-            send_data =transfer_quota(data=data, dbms=args.dbms) 
-            post_data(conn=args.conn, data=send_data) 
- 
-            time.sleep(args.sleep) 
+    for i in range(args.iteration): 
+        data = process(token=args.token, dbms=args.dbms)
+        for key in data: 
+            for row in data[key]: 
+                post_data(conn=args.conn, data=row)
+        time.sleep(args.sleep) 
 
 if __name__ == '__main__':
     main() 
