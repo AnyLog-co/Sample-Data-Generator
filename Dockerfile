e1624f691634feae26c30fbabcc59b8a4a2f40a0
Oi Shadmon <oshadmon@gmail.com>
2022-02-01 08:58:00 -0800

    in progress dockerize

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,28 @@
-FROM ubuntu:18.04
+FROM ubuntu:20.04
 
-# declare params 
-ARG ANYLOG_ROOT_DIR=/app
+# declare params
+ENV ANYLOG_ROOT_DIR=/app
+ENV DEBIAN_FRONTEND=noninteractive
 
-# update / upgrade 
-RUN apt-get -y update 	
-RUN apt-get -y upgrade 
-RUN apt-get -y update	
+# update / upgrade
+RUN apt-get -y update
+RUN apt-get -y upgrade
+RUN apt-get -y update
 
-# install requirements via apt 
-RUN apt-get -y install python3 python3-pip
-RUN pip3 install --upgrade pip
+# install requirements via apt
+RUN apt-get -y install curl git
+RUN apt-get -y install python3.9 python3-pip
+RUN apt-get -y install libpq-dev python3.9-dev
+RUN apt-get -y install python3-kafka
+RUN python3.9 -m pip install --upgrade pip
 
 # install requirements via pip 
-RUN python3 -m pip install requests
-RUN python3 -m pip install pytz
-RUN python3 -m pip install paho-mqtt
-RUN python3 -m pip install tzlocal
-
+RUN python3.9 -m pip install requests
+RUN python3.9 -m pip install pytz
+RUN python3.9 -m pip install paho-mqtt
+RUN python3.9 -m pip install tzlocal
+RUN python3.9 -m pip install kafka-python
+RUN apt-get -y update
 
 # Add user 
 RUN adduser appuser 
@@ -31,7 +36,8 @@ RUN chown -R appuser:appuser $ANYLOG_ROOT_DIR
 RUN chmod 755 /app
 
 # Swtich user 
-USER appuser 
+USER appuser
+RUN apt-get -y update
 
-ENTRYPOINT bash /app/Sample-Data-Generator/docker_call.sh
+#ENTRYPOINT bash /app/Sample-Data-Generator/docker_call.sh
  
--- docker_call.sh
+++ docker_call.sh
@@ -1,33 +1,18 @@
 #!/bin/bash
 
-error=0
+if [[ ! ${RPEAT} ]] ; then REPEAT=1 ; fi
+if [[ ! ${SLEEP} ]] ; then  SLEEP=1 ; fi
+if [[ ! ${BATCH_REPEAT} ]] ; then BATCH_REPEAT=10 ; fi
+if [[ ! ${BATCH_SLEEP} ]] ; then BATCH_SLEEP=0.5 ; fi
 
-if [[ ${HELP} == true ]] ; then python3 /app/Sample-Data-Generator/data_generator.py --help ; exit 1 ; fi
+#    parser.add_argument('--timezone', type=str, choices=['local', 'UTC', 'ET', 'BR', 'JP', 'WS', 'AU', 'IT'], default='local', help='timezone for generated timestamp(s)')
+ #    parser.add_argument('--enable-timezone-range', type=bool, nargs='?', const=True, default=False, help='whether or not to set timestamp within a "range"')
+ #    parser.add_argument('--authentication', type=str, default=None, help='username, password')
+ #    parser.add_argument('--timeout', type=float, default=30, help='REST timeout (in seconds)')
+ #    parser.add_argument('--topic', type=str, default=None, help='topic for either REST POST or MQTT')
 
-# validate base params
-if [[ ${HELP} == true ]] ; then python3 data_generator.py --help ; exit 1 ; fi
-
-if [[ ! ${CONN} ]] ; then echo Missing connection information ; error=1 ; fi
-if [[ ! ${GENERATOR} ]] ; then echo Missing data generator information ; error=1 ; fi
-if [[ ! ${SAVE} ]] ; then echo Missing format to save content as ; error=1 ; fi
-if [[ ! ${DBMS} ]] ; then echo Missing logical database name ; error=1 ; fi
-
-if [[ ${error} != 0 ]] ; then exit 1 ; fi
-
-# Optional PARAMS
-if [[ ${REPEAT} ]] ; then REPEAT=`echo --repeat ${REPEAT}` ; fi
-if [[ ${SLEEP} ]] ; then SLEEP=`echo --sleep ${SLEEP}` ; fi
-if [[ ${BATCH_REPEAT} ]] ; then BATCH_REPEAT=`echo --batch-repeat ${BATCH_REPEAT}` ; fi
-if [[ ${BATCH_SLEEP} ]] ; then BATCH_SLEEP=`echo --batch-sleep ${BATCH_SLEEP}` ; fi
-if [[ ${TIMEZONE} ]] ; then TIMEZONE=`echo --timezone ${TIMEZONE}`; fi
-if [[ ${AUTHENTICATION} ]] ; then AUTHENTICATION=`echo --authentication ${AUTHENTICATION}` ; fi
-if [[ ${TIMEOUT} ]] ; then TIMEOUT=`echo --timeout ${TIMEOUT}` ; fi
-if [[ ${TOPIC} ]] ; then TOPIC=`echo --topic ${TOPIC}` ; fi
-if [[ ${TOKEN} ]] ; then TOKEN=`echo --linode-token ${TOKEN}` ; fi
-if [[ ${TAG} ]] ; then TAG=`echo --linode-tag ${TAG}` ; fi
-
-if [[ ! ${EXCEPTION} == true ]] ; then
-  python3 /app/Sample-Data-Generator/data_generator.py ${CONN} ${GENERATOR} ${SAVE} ${DBMS} ${REPEAT} ${SLEEP} ${BATCH_REPEAT} ${BATCH_SLEEP} ${TIMEZONE} ${AUTHENTICATION} ${TIMEOUT} ${TOPIC} ${TOKEN} ${TAG}
-else
-  python3 /app/Sample-Data-Generator/data_generator.py ${CONN} ${GENERATOR} ${SAVE} ${DBMS} ${REPEAT} ${SLEEP} ${BATCH_REPEAT} ${BATCH_SLEEP} ${TIMEZONE} ${AUTHENTICATION} ${TIMEOUT} ${TOPIC} ${TOKEN} ${TAG} -e
-fi
+if [[ ! ${TIMEZONE} ]] ; then TIMEZONE=local ; fi
+if [[ ! ${ENABLE_TIMEZONE_RANGE} ]] ; then ENABLE_TIMEZONE_RANGE=false ; fi
+if [[ ! ${AUTHENTICATION} ]] ; then AUTHENTICATION='' ; fi
+if [[ ! ${TIMEOUT} ]] ; then TIMEZONE=30 ; fi
+if [[ ! ${PROTOCOL} == put ]] && [[ ! ${PROTOCOL} == print ]] && [[ ! ${PROTOCOL} == file ]] && [[ ! ${TOPIC}]] ; then echo "TOPIC required for protocol: ${PROTOCOL}" ; fi
