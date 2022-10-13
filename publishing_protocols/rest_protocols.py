204b716208d56d5573d7b821bfc77dbac5143906
Ori Shadmon <oshadmon@gmail.com>
2022-10-12 18:26:04 -0700

    working code for data generator(s)

--- publishing_protocols/rest_protocols.py
+++ publishing_protocols/rest_protocols.py
@@ -92,19 +92,21 @@ def post_data(payloads:list, conn:str, topic:str="demo", auth:tuple=(), timeout:
         'Content-Type': 'text/plain'
     }
 
-    for payload in payloads:
-        str_payloads = support.json_dumps(payloads=payload)
-        try:
-            r = requests.post(url=f'https://{conn}', headers=headers, auth=auth, timeout=timeout, data=str_payloads)
-        except Exception as error:
+    str_payloads = support.json_dumps(payloads=payloads)
+    try:
+        r = requests.post(url=f'http://{conn}', headers=headers, auth=auth, timeout=timeout, data=str_payloads)
+    except Exception as error:
+        status = False
+        if exception is True:
+            print(f'Failed to execute POST against {conn} (Error: {error})')
+        exit(1)
+    else:
+        if int(r.status_code) != 200:
             status = False
             if exception is True:
-                print(f'Failed to execute POST against {conn} (Error: {error})')
-        else:
-            if int(r.status_code) != 200:
-                status = False
-                if exception is True:
-                    print(f'Failed to execute POST against {conn} (Network Error: {r.status_code})')
+                print(f'Failed to execute POST against {conn} (Network Error: {r.status_code})')
+            exit(1)
+
 
     return status
 
