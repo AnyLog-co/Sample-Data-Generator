1f545151f205b46738d2fc65c50b9fc68ab33e90
Ori Shadmon <oshadmon@gmail.com>
2022-10-13 10:47:54 -0700

    able to send videos

--- data_generator_video_image_processing.py
+++ data_generator_video_image_processing.py
@@ -7,17 +7,18 @@ import uuid
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 DATA_GENERATORS = os.path.join(ROOT_PATH, 'data_generators')
-PROTOCOLS = os.path.join(ROOT_PATH, 'protocols')
+PROTOCOLS = os.path.join(ROOT_PATH, 'publishing_protocols')
 sys.path.insert(0, DATA_GENERATORS)
 sys.path.insert(0, PROTOCOLS)
 
 import video_image_processing
+import publish_data
 import support
 
 PROCESS_ID = str(uuid.uuid4())
 
 
-DIR_NAME='$HOME/Sample-Data-Generator/data/videos'
+DIR_NAME='$HOME/Downloads/videos'
 DEVICE_NAME='anylog-data-generator'
 PROFILE_NAME='anylog-video-generator'
 
@@ -75,72 +76,6 @@ def __car_counter(timestamp:datetime.datetime)->(int, float):
     return speed, cars
 
 
-def store_data(protocol:str, payload_data:dict, dbms:str='edgex', table:str='images', conn:str=None,
-               topic:str='anylogedgex', data_dir:str=os.path.join(ROOT_PATH, 'data'), auth:str=None, timeout:float=30,
-               compress:bool=False, exception:bool=False):
-    """
-    Store content based on the selected protocol(s)
-    :args:
-        protocol:str - format to save data
-        payloads:dict - content to store
-        dbms:str - logical database to store data in
-        conn:str - REST IP + Port or broker IP + Port
-        auth:str - username, password
-        timeout:float - REST timeout (in seconds)
-        topic:str - MQTT / REST POST topic
-        data_dir:str - for data_generator type file directory containing data to read
-        compress:bool - whether the content in data_dir is compressed
-        exception:bool - whether or not to print exceptions
-    :params:
-        table:str - table name
-        broker:str - broker for MQTT
-        port:str - port for MQTT
-        username:str - username for MQTT
-        password:str - password for MQTT
-        mqtt_conn:paho.mqtt.client.Client - MQTT client connection
-
-    """
-    # convert data to JSON
-    timestamp = payload_data['origin']
-    payloads = support.json_dumps(data=payload_data)
-    if not isinstance(payloads, str):
-        print(f'Failed to convert content to JSON string for file.')
-        return
-
-    # Send data
-    if protocol == 'file':
-        import generic_protocol
-        status, file_path = generic_protocol.write_to_file(data=payloads, dbms=dbms, table=dbms, timestamp=timestamp,
-                                                           data_dir=data_dir, compress=compress, exception=exception)
-        if status is False:
-            print(f'Failed to write data into file: {os.path.join(data_dir, dbms)}.{table}.json')
-        else:
-            print(f'Data is stored in: {file_path}')
-    elif protocol == 'post':
-        import rest
-        if not rest.post_data(conn=conn, payload=payloads, rest_topic=topic, auth=auth,
-                              timeout=timeout, exception=exception):
-            print(f'Failed to POST data against {conn}')
-    elif protocol == 'mqtt':
-        import mqtt
-        broker, port = conn.rstrip().lstrip().replace(' ', '').split(':')
-        user = ''
-        password = ''
-        if auth is not None:
-            user, password = auth.rstrip().lstrip().replace(' ', '').split(',')
-        mqtt_conn = mqtt.connect_mqtt_broker(broker=broker, port=port, username=user, password=password)
-        if mqtt_conn is not None:
-            if not mqtt.send_data(mqtt_client=mqtt_conn, topic=topic, message=payloads, exception=exception):
-                print(f'Failed to send data via MQTT against {conn}')
-    elif protocol == 'kafka':
-        import kafka_protocol as kafka
-        servers = conn.split(',')
-        kafka_conn = kafka.connect_kafka(servers=servers, exception=exception)
-        if kafka_conn is not None:
-            if not kafka.publish_data(producer=kafka_conn, topic=topic, data=payloads, dbms=dbms, table=table,
-                                      exception=exception):
-                print(f'Failed to send data via Kafaka against {conn}')
-
 
 def main():
     """
@@ -187,6 +122,9 @@ def main():
     args = parser.parse_args()
 
     dirs_full_path = os.path.expandvars(os.path.expanduser(DIR_NAME))
+    if not os.path.isdir(dirs_full_path):
+        print(f'Failed to locate directory {DIR_NAME}, cannot continue')
+        exit(1)
     list_dirs = os.listdir(dirs_full_path)
     reversed_list_dirs = list(reversed(list_dirs))
     now = datetime.datetime.strptime('2022-07-30T00:00:00.000000Z', '%Y-%m-%dT%H:%M:%S.%fZ')
@@ -206,9 +144,8 @@ def main():
                                                        device_name=DEVICE_NAME, profile_name=PROFILE_NAME,
                                                        start_ts=start_ts, end_ts=end_ts, speed=speed, cars=cars,
                                                        exception=args.exception)
-            store_data(protocol=args.protocol, payload_data=processed_data, dbms=args.dbms, table=args.table,
-                       conn=conn, topic=args.topic, data_dir=os.path.join(ROOT_PATH, 'data'), auth=args.authentication,
-                       timeout=args.timeout, compress=args.compress, exception=args.exception)
+            publish_data.publish_data(payload=processed_data, insert_process=args.protocol, conn=conn, topic=args.topic,
+                                      compress=True, rest_timeout=30, exception=args.exception)
         now = end_ts
 
 
