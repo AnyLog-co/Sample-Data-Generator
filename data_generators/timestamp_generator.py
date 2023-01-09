605fab60320da262f85c37c816fb44b3a69a3b00
Ori Shadmon <oshadmon@gmail.com>
2023-01-09 10:21:50 -0800

    file generator

--- data_generators/timestamp_generator.py
+++ data_generators/timestamp_generator.py
@@ -3,42 +3,7 @@ import pytz
 import random
 
 
-def __timestamp_string(timestamp:datetime.datetime)->str:
-    """
-    Convert timestamp to string
-    :args:
-        timestamp:datetime.datetime - timestamp
-    :return:
-        timestamp as string format
-    """
-    try:
-        timestamp = timestamp.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-    except Exception as error:
-        pass
-
-    return timestamp
-
-def performance_timestamp(microseconds:int, second_increments:float)->str:
-    """
-    Calculate timestamp for performance testing
-    :args:
-        microseconds:str - base microseconds (calculated as global value)
-        second_increments:str - how much to increment by
-    :params:
-       timestamp:datetime.datetime - calculated timestamp
-    :return:
-         timestamp as string
-    """
-    # calculate the base timestamp
-    timestamp = datetime.datetime(year=2022, month=8, day=27, hour=15, minute=50, second=12) + datetime.timedelta(microseconds=microseconds)
-    
-    # update based on second_increments
-    timestamp += datetime.timedelta(seconds=second_increments)
-    
-    return __timestamp_string(timestamp=timestamp)
-
-
-def generate_timestamp(timezone:str='utc', enable_timezone_range:bool=True)->str:
+def __generate_timestamp(timezone:str='utc', enable_timezone_range:bool=True)->datetime.datetime:
     """
     generate timestamp based on timezone
     :args:
@@ -74,18 +39,69 @@ def generate_timestamp(timezone:str='utc', enable_timezone_range:bool=True)->str
         if enable_timezone_range is True:
             timestamp += datetime.timedelta(days=random.choice(range(-30, 31)), hours=random.choice(range(-23, 24)),
                                             minutes=random.choice(range(-59, 60)))
-        timestamp = timestamp.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
     else: # actual utc value
         timestamp = datetime.datetime.utcnow()
         if enable_timezone_range is True:
             timestamp += datetime.timedelta(days=random.choice(range(-30, 31)), hours=random.choice(range(-23, 24)),
                                             minutes=random.choice(range(-59, 60)))
-        timestamp = timestamp.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
 
+    return timestamp
+
+
+def __timestamp_string(timestamp:datetime.datetime)->str:
+    """
+    Convert timestamp to string
+    :args:
+        timestamp:datetime.datetime - timestamp
+    :return:
+        timestamp as string format
+    """
+    try:
+        timestamp = timestamp.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    except Exception as error:
+        pass
+
+    return timestamp
+
+
+def performance_timestamp(microseconds:int, second_increments:float)->str:
+    """
+    Calculate timestamp for performance testing
+    :args:
+        microseconds:str - base microseconds (calculated as global value)
+        second_increments:str - how much to increment by
+    :params:
+       timestamp:datetime.datetime - calculated timestamp
+    :return:
+         timestamp as string
+    """
+    # calculate the base timestamp
+    timestamp = datetime.datetime(year=2022, month=8, day=27, hour=15, minute=50, second=12) + datetime.timedelta(microseconds=microseconds)
+    
+    # update based on second_increments
+    timestamp += datetime.timedelta(seconds=second_increments)
+    
     return __timestamp_string(timestamp=timestamp)
 
 
-def cars_timestamps()->(datetime.datetime, datetime.datetime):
+def generate_timestamp(timezone:str='utc', enable_timezone_range:bool=True)->str:
+    """
+    generate timestamp based on timezone
+    :args:
+        timezone:str - timezone either utc or other
+        enable_timezone_range:bool - if set alter timestamp by datetime.timedelta(days=random.choice(range(-30, 31)),
+                                                                                  hours=random.choice(range(-23, 24)),
+                                                                                  minutes=random.choice(range(-59, 60)))
+    :params:
+        timestamp:str - generated timestamp
+    :return:
+        timestamp
+    """
+    timestamp = __generate_timestamp(timezone=timezone, enable_timezone_range=enable_timezone_range)
+    return __timestamp_string(timestamp=timestamp)
+
+
+def cars_timestamps(timezone:str, enable_timezone_range:bool=False)->(datetime.datetime, datetime.datetime):
     """
     Generate 2 timestamps that are 5 to 90 seconds apart - used in car_images data generator
     :params:
@@ -94,7 +110,7 @@ def cars_timestamps()->(datetime.datetime, datetime.datetime):
     :return: 
         timestamp, timestamp2
     """
-    timestamp = datetime.datetime.utcnow()
+    timestamp = __generate_timestamp(timezone=timezone, enable_timezone_range=enable_timezone_range)
     timestamp2 = timestamp + datetime.timedelta(seconds=random.choice(range(5, 90)))
     
     return timestamp, timestamp2
