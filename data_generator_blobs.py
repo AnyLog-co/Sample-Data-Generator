85506d37ebcee6a79f6de58715fdda2b9bd7afa8
Ori Shadmon <oshadmon@gmail.com>
2023-02-23 14:27:18 -0800

    notes + reorg

--- data_generator_blobs.py
+++ data_generator_blobs.py
@@ -12,86 +12,87 @@ import data_generators.data_generator_videos as data_generator_videos
 import publishing_protocols.publish_data as publish_data
 from data_generators.file_processing import check_conversion_type
 
+DATA_DIR = os.path.join(ROOT_PATH, 'data')
+
+
 def main():
     """
     The following provides an example for storing blobs (ex. images and videos) with associated values into AnyLog.
-    The data set for this data generator can be downloaded here: https://drive.google.com/drive/folders/1EuArx1VepoLj3CXGrCRcxzWZyurgUO3u?usp=share_link
-    :note:
-        on the AnyLog side, make sure MongoDB is running and associated `run mqtt client` is active. Otherwise, data
-        will not come in and/or will not be stored
+    * video data is associated with traffic data [data_generators/car_insight.py)
+    * image data is associated with an error on the image, if found
+    :data-sets:
+        https://drive.google.com/drive/folders/1EuArx1VepoLj3CXGrCRcxzWZyurgUO3u?usp=share_link
     :positional arguments:
-        dir_name    directory where files are stored
-        conn        {user}:{password}@{ip}:{port} for sending data either via REST or MQTT
-        protocol    format to save data
-            * post
-            * mqtt
-            * print
-    :options:
-        -h, --help                show this help message and exit
-        --topic     TOPIC         topic to send data agaisnt
-        --table     TABLE         Logical database to store data in
-        --sleep     SLEEP         Wait time between each file to insert
-        --timeout   TIMEOUT       REST timeout (in seconds)
-        --timezone  TIMEZONE      timezone for generated timestamp(s)
-            * local
-            * utc
-            * et
-            * br
-            * jp
-            * ws
-            * au
-            * it
-        --enable-timezone-range     [ENABLE_TIMEZONE_RANGE]     set timestamp within a range of +/- 1 month
-        --conversion-type           CONVERSION_TYPE             Format to convert file to - cv2 can be used for live camera feed
-            * base64
-            * bytesio
-            * cv2
-        --reverse                   [REVERSE]                   whether to store data in reversed (file) order
-        --exception                 [EXCEPTION]                 whether to print exceptions to screen
+        dir_name              directory where files are stored - data is generated based on the file
+        protocol              format to save data
+        * post
+        * mqtt
+        * print
+        * put
+        * file
+        conversion_type       Format to convert file to - cv2 can be used for live camera feed
+        * base64
+        * bytesio
+        * opencv
+    :optional arguments:
+        -h, --help                      show this help message and exit
+        --db-name       DB_NAME         Logical database to store data in
+        --table         TABLE           Logical database to store data in
+        --sleep         SLEEP           Wait time between each file to insert
+        --conn          CONN            {user}:{password}@{ip}:{port} for sending data either via REST or MQTT
+        --topic         TOPIC           topic to send data agaisnt
+        --timeout       TIMEOUT         REST timeout (in seconds)
+        --timezone      TIMEZONE        timezone for generated timestamp(s)
+        * local
+        * utc
+        * et
+        * br
+        * jp
+        * ws
+        * au
+        * it
+        --enable-timezone-range [ENABLE_TIMEZONE_RANGE]     set timestamp within a range of +/- 1 month
+        --reverse               [REVERSE]                   whether to store data in reversed (file) order
+        --results-dir           RESULTS_DIR                 directory when storing to file
+        --compress              [COMPRESS]                  whether to zip data dir
+        --exception             [EXCEPTION]                 whether to print exceptions
+    :params:
+        sub_dir:str - whether it's data from videos or images
+        conns:dict - list of connections with  access information
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('dir_name', type=str, default='$HOME/Downloads/sample_data/videos',
                         help='directory where files are stored - data is generated based on the file')
-    parser.add_argument('conn', type=str, default='127.0.0.1:32149',
-                        help='{user}:{password}@{ip}:{port} for sending data either via REST or MQTT')
-    parser.add_argument('protocol', type=str, choices=['post', 'mqtt', 'print', 'put'], default='post',
+    parser.add_argument('protocol', type=str, choices=['post', 'mqtt', 'print', 'put', 'file'], default='post',
                         help='format to save data')
-    parser.add_argument('--topic', type=str, default='anylog-data-gen', help='topic to send data agaisnt')
+    parser.add_argument('conversion_type', type=check_conversion_type, default='base64',
+                        choices=['base64', 'bytesio', 'opencv'],
+                        help='Format to convert file to - cv2 can be used for live camera feed')
     parser.add_argument('--db-name', type=str, default='edgex', help='Logical database to store data in')
     parser.add_argument('--table', type=str, default='image', help='Logical database to store data in')
     parser.add_argument('--sleep', type=float, default=5, help='Wait time between each file to insert')
+    parser.add_argument('--conn', type=str, default=None,
+                       help='{user}:{password}@{ip}:{port} for sending data either via REST or MQTT')
+    parser.add_argument('--topic', type=str, default='anylog-data-gen', help='topic to send data agaisnt')
     parser.add_argument('--timeout', type=float, default=30, help='REST timeout (in seconds)')
     parser.add_argument('--timezone', type=str, choices=['local', 'utc', 'et', 'br', 'jp', 'ws', 'au', 'it'],
                         default='local', help='timezone for generated timestamp(s)')
     parser.add_argument('--enable-timezone-range', type=bool, nargs='?', const=True, default=False,
                         help='set timestamp within a range of +/- 1 month')
-    parser.add_argument('--conversion-type', type=check_conversion_type, default='base64', choices=['base64', 'bytesio', 'cv2'],
-                        help='Format to convert file to - cv2 can be used for live camera feed')
     parser.add_argument('--reverse', type=bool, nargs='?', const=True, default=False,
                         help='whether to store data in reversed (file) order')
+    parser.add_argument('--results-dir', type=str, default=DATA_DIR, help='directory when storing to file')
+    parser.add_argument('--compress', type=bool, nargs='?', const=True, default=False, help='whether to zip data dir')
     parser.add_argument('--exception', type=bool, nargs='?', const=True, default=False,
-                        help='whether to print exceptions to screen')
+                       help='whether to print exceptions')
     args = parser.parse_args()
 
-    # validate directory exists
+    # validate directory exists + if so, get sub_ddir (videos || images)
     args.dir_name = os.path.expanduser(os.path.expandvars(args.dir_name))
     if not os.path.isdir(args.dir_name):
         print(f"Failed to locate data directory {args.dir_name}, cannot continue...")
         exit(1)
-
-    conns = None
-    if args.conn is not None:
-        conns = args.conn.split(',')
-
-    if args.protocol == "mqtt":
-        conns = publish_data.connect_mqtt(conns, exception=args.exception)
-        if not conns:
-            print("Failed to set connection for MQTT publisher")
-            exit(1)
-    elif args.protocol in ["post", "put"]:
-        conns = publish_data.setup_put_post_conn(conns=conns)
-
-    if 'win32' in sys.platform and args.dir_name[-1] == '\\':
+    elif 'win32' in sys.platform and args.dir_name[-1] == '\\':
         sub_dir = args.dir_name[:-1].rsplit('\\')[-1]
     elif 'win32' in sys.platform:
         sub_dir = args.dir_name.rsplit('\\')[-1]
@@ -99,6 +100,17 @@ def main():
         sub_dir = args.dir_name[:-1].rsplit('/')[-1]
     else:
         sub_dir = args.dir_name.rsplit('/')[-1]
+
+    conns = None
+    if args.conn is not None:
+        conns = args.conn.split(',')
+        if args.protocol == "mqtt":
+            conns = publish_data.connect_mqtt(conns, exception=args.exception)
+            if not conns:
+                print("Failed to set connection for MQTT publisher")
+                exit(1)
+        elif args.protocol in ["post", "put"]:
+            conns = publish_data.setup_put_post_conn(conns=conns)
 
     if sub_dir == "videos":
         data_generator_videos.main(dir_name=args.dir_name, conns=conns, protocol=args.protocol, topic=args.topic,
