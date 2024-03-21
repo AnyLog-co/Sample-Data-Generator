827137796a66c9a9008b560aeaa6719eaf0d00a4
Ori Shadmon <oshadmon@gmail.com>
2024-03-20 18:10:10 -0700

    reorg
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

---
+++ data_generator/blobs_car_video.py
@@ -0,0 +1,94 @@
+import os
+import random
+import time
+import uuid
+
+import data_generator.support as support
+from data_generator.video_processing import VideoProcessing
+
+ROOT_PATH = os.path.expandvars(os.path.expanduser(__file__)).split("data_generator")[0]
+BLOBS_DIR  = os.path.join(ROOT_PATH, 'blobs', 'car_video')
+MODEL_FILE = os.path.join(ROOT_PATH, 'blobs', 'models', 'vehicle.tflite')
+PROCESS_ID = str(uuid.uuid4())
+
+
+def __car_counter(file_path:str, exception:bool=False):
+    num_cars = []
+    speeds = []
+
+    vp = VideoProcessing(model_file=MODEL_FILE, labels=[f"0 car"], img_process='vehicle', video=file_path,
+                         exception=exception)
+    vp.set_interpreter()
+    vp.process_video(min_confidence=0)
+    cars, speed = vp.get_values()
+    num_cars.append(cars)
+    if 'A.mp4' in file_path:
+        speeds.append(speed * 10)
+    else:
+        speeds.append(speed * 100)
+
+    avg_speed = sum(speeds)/len(speeds)
+    if 'A.mp4' in file_path:
+        return int(sum(num_cars)/100), round(avg_speed * 10, 2)
+    else:
+        return int(int(sum(num_cars) / len(num_cars)) / 100), round(sum(speeds)/len(speeds) * 10, 2)
+
+
+def __create_data(binary_file:str, file_name:str, db_name:str, num_cars:int, speed:float,
+                  table_name:str='car_videos', profile_name="anylog-video-generator"):
+    data = {
+        "apiVersion": "v2",
+        "dbName": db_name,
+        "id": PROCESS_ID,
+        "deviceName": table_name,
+        "origin": int(time.time()),
+        "profileName": profile_name,
+        "readings": [],
+        "sourceName": "OnvifSnapshot"
+    }
+
+    # file_name = file_name
+    data["readings"].append({
+        "timestamp": support.create_timestamp(increase_ts=0),
+        "start_ts": support.create_timestamp(increase_ts=0),
+        "end_ts": support.create_timestamp(increase_ts=random.choice(list(range(10, 60)))),
+        "binaryValue": binary_file,
+        "deviceName": file_name.split(".")[0],
+        "id": support.generate_string_hash(file_name=file_name, data=binary_file),
+        "mediaType": support.media_type(file_suffix=file_name.rsplit(".", 1)[-1]),
+        "origin": int(time.time()),
+        "profileName": file_name.split(".")[1],
+        "resourceName": "OnvifSnapshot",
+        "valueType": "Binary",
+        "num_cars": num_cars,
+        "speed": speed
+    })
+
+    return data
+
+
+
+def car_counting(db_name:str, row_count:int, conversion_type:str="base64", sleep:float=0.5, timezone:str="local",
+                   last_blob:str=None, enable_timezone_range:bool=False, exception:bool=False):
+    payloads = []
+
+    if not os.path.isdir(BLOBS_DIR):
+        print(f"Failed to locate directory with images/videos ({BLOBS_DIR}), cannot continue...")
+        exit(1)
+
+    video = None
+    for i in range(row_count):
+        while video == last_blob or video is None:
+            video = random.choice(list(os.listdir(BLOBS_DIR)))
+            full_file_path = os.path.expanduser(os.path.expandvars(os.path.join(BLOBS_DIR, video)))
+            if not os.path.isfile(full_file_path):
+                video = None
+
+        num_cars, avg_speed = __car_counter(file_path=full_file_path, exception=exception)
+
+        binary_file = support.file_processing(file_name=full_file_path, exception=exception)
+        payloads.append(__create_data(binary_file=binary_file, file_name=video, db_name=db_name,
+                                      num_cars=num_cars, speed=avg_speed))
+        last_blob  = video
+
+    return payloads, last_blob
--- data_generator/support.py
+++ data_generator/support.py
@@ -1,10 +1,98 @@
+import base64
 import datetime
+import hashlib
 import json
+import uuid
 
 
-def create_timestamp():
-    return datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-
+def create_timestamp(increase_ts:float=0):
+    timestamp = datetime.datetime.utcnow() + datetime.timedelta(seconds=increase_ts)
+    return timestamp.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
 
 def serialize_data(payload):
     return json.dumps(payload)
+
+
+def generate_string_hash(file_name:str, data:str)->str:
+    """
+    based on file_name + data generate hash value
+    :args:
+        file_name:str - file used to generate has value
+        data:str - content in file
+    :params:
+        str_hash:hashlib.Hash - generated hash value
+    :return:
+        hash value as UUID
+    """
+    str_hash = hashlib.md5()
+    if file_name:
+        # Prefixed data can be dbms name and table name that are considered in the hash
+        str_hash.update(file_name.encode())  # Update the hash with the prefix data+
+
+    str_hash.update(data.encode())  # Update the hash
+    return str(uuid.UUID(str_hash.hexdigest()))
+
+def media_type(file_suffix:str)->str:
+    """
+    Generate media type based on file suffix
+    :args:
+        file_suffix:str - file suffix
+    :params:
+        suffix_value:str - based on file suffix generated suffix
+    :return:
+        suffix_value
+    """
+    suffix_value = 'unknown'
+    if file_suffix == 'png':
+        suffix_value = 'image/png'
+    elif file_suffix in ['jpg', 'jpeg']:
+        suffix_value = 'image/jpeg'
+    elif file_suffix == 'mp4':
+        suffix_value = 'video/mp4'
+
+    return suffix_value
+
+def file_processing(file_name:str, exception:bool=False):
+    """
+    read content from file and convert to base64 - used for both images and blobs
+    :steps:
+        1. read file
+        2. base64 encode file
+        3. base64 decode ASCII
+    :args:
+        file_name:str - file to process
+        exception:bool - whether to print exceptions
+    :params:
+
+        file_content - content from file
+        full_file_path:str - full path of file_name
+    """
+    file_content = None
+    base64_bytes = None
+    full_file_path = os.path.expanduser(os.path.expandvars(file_name))
+
+    try:
+        with open(file_name, 'rb') as f:
+            try:
+                content  = f.read()
+            except Exception as error:
+                if exception is True:
+                    print(f"Failed to read content from file {file_name} (Error: {error})")
+    except Exception as error:
+        if exception is True:
+            print(f"Failed to open file {file_name} to be read (Error: {error})")
+    else:
+        try:
+            base64_bytes = base64.b64encode(content)
+        except Exception as error:
+            status = False
+            if exception is True:
+                print(f'Failed to encode file data (Error: {error})')
+        else:
+            try:
+                file_content = base64_bytes.decode('ascii')
+            except Exception as error:
+                if exception is True:
+                    print(f'Failed to convert encoded message to ASCII based value (Error: {error})')
+
+        return file_content
