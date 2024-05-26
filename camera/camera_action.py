49172bb5279412c9d2ae06ba5a4b715f98b62c73
Ori Shadmon <oshadmon@gmail.com>
2024-05-25 23:53:18 -0700

    contiouse livefeed - start/stop image
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- camera/camera_action.py
+++ camera/camera_action.py
@@ -1,31 +1,87 @@
+import argparse
 import cv2
 
-# Open a connection to the webcam (0 is the default camera)
-cap = cv2.VideoCapture(0)
+class VideoDisplay:
+    def __init__(self, camera_id=0, width=640, height=480):
+        self.camera_id = camera_id
+        self.width = width
+        self.height = height
+        self.is_running = False
+        self.cap = self.__enable_video_capure()
+        if not self.cap.isOpened() or self.cap is None:
+            print("Error: Could not open video device.")
+            exit()
 
-if not cap.isOpened():
-    print("Error: Could not open video device.")
-    exit()
+    def __enable_video_capure(self):
+        cap = None
+        try:
+            cap = cv2.VideoCapture(self.camera_id)
+        except Exception as error:
+            print(f"Failed to start video capture with cemra {self.camera_id} (Error: {error})")
+        return cap
 
-# Set video frame width and height
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    def __set_cap_size(self, height:int=None, width:int=None):
+        if height is None:
+            height = self.height
+        if width is None:
+            width = self.width
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-    if not ret:
-        print("Error: Could not read frame.")
-        break
+    def start(self, height:int=None, width:int=None):
+        self.__set_cap_size(height=height, width=width)
 
-    # Display the resulting frame
-    cv2.imshow('Video Feed', frame)
+        self.is_running = True
+        while self.is_running:
+            ret, frame = self.cap.read()
+            if not ret:
+                print("Error: Could not read frame.")
+                break
 
-    # Exit the video window by pressing 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            cv2.imshow('Video Feed', frame)
 
-# Release the capture and close any OpenCV windows
-cap.release()
-cv2.destroyAllWindows()
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        self.stop()
+
+    def stop(self):
+        self.is_running = False
+        # self.cap.release()
+        cv2.destroyAllWindows()
+
+    def destory_windows(self):
+        self.cap.release()
+
+
+def main():
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--camera-id', type=int, default=0, help='Camera ID')
+    parse.add_argument('--width', type=float, default=640, help='livefeed screen ratio width')
+    parse.add_argument('--height', type=float, default=480, help='livefeed screen ratio height')
+    parse.add_argument('--show-video', type=bool, nargs='?', const=True, default=False, help='begin with an active livefeed')
+    args = parse.parse_args()
+
+    video_display = VideoDisplay(camera_id=args.camera_id, width=args.width, height=args.height)
+    command = None
+    if args.show_video is True:
+        command = 'o'
+
+    while True:
+        if command == 'o':  # show video
+            video_display.start(height=args.height, width=args.width)
+        elif command == 'q':  # quit
+            video_display.destory_windows()
+            break
+        elif command == 'h': # update height
+            args.height = float(input("Updated height: "))
+        elif command == 'w':
+            args.width = float(input("Updated width: "))
+        elif command is not None:
+            print(f"Invalid option {command}")
+        command = input("Command: ").strip()
+
+
+if __name__ == "__main__":
+    main()
