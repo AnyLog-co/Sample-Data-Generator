fa98048aed64ef3f45b16b8ebba8b8079b6b26c1
Your Name <you@example.com>
2026-03-20 01:39:26 +0000

    tmp
    Signed-off-by: Your Name <you@example.com>

--- mqtt_run.sh
+++ mqtt_run.sh
@@ -1,27 +1,32 @@
+#!/bin/bash
+
 # Vessel Data
-python3 $HOME/Sample-Data-Generator/venv/data_generator_main.py \
+/root/Sample-Data-Generator/venv/bin/python3 $HOME/Sample-Data-Generator/data_generator_main.py \
   --data-conn anyloguser:mqtt4AnyLog!@172.104.228.251:1883 \
   --db-name anotherpeak \
   --skip-msg-client \
-  --repeat 0 vessel mqtt
+  --repeat 0 vessel mqtt &
 
 # Rig Data
-python3 $HOME/Sample-Data-Generator/venv/data_generator_main.py \
+/root/Sample-Data-Generator/venv/bin/python3 $HOME/Sample-Data-Generator/data_generator_main.py \
   --data-conn anyloguser:mqtt4AnyLog!@172.104.228.251:1883 \
   --db-name timbergrove_rigs \
   --skip-msg-client \
-  --repeat 0 rig mqtt
+  --repeat 0 rig mqtt &
 
 # Wind Turbine
-python3 $HOME/Sample-Data-Generator/venv/data_generator_main.py \
+/root/Sample-Data-Generator/venv/bin/python3 $HOME/Sample-Data-Generator/data_generator_main.py \
   --data-conn anyloguser:mqtt4AnyLog!@172.104.228.251:1883 \
   --db-name wind_turbine \
   --skip-msg-client \
-  --repeat 0 wind-turbine mqtt
+  --repeat 0 wind-turbine mqtt &
 
 # Random Data
-python3 $HOME/Sample-Data-Generator/venv/data_generator_main.py \
+/root/Sample-Data-Generator/venv/bin/python3 $HOME/Sample-Data-Generator/data_generator_main.py \
   --data-conn anyloguser:mqtt4AnyLog!@172.104.228.251:1883 \
   --db-name mydb \
   --skip-msg-client \
-   --repeat 0 random mqtt
+  --repeat 0 random mqtt &
+
+# IMPORTANT: keep service running
+wait
